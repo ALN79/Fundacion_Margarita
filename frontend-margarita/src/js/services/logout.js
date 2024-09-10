37bbd939d987ee1e8fe20d51cc872c6b3b8d3cce
Nahuel <nahuelcarlos150@gmail.com>
2024-09-10 14:35:43 -0300

    se hizo autenticacion de jwt y logout

--- frontend-margarita/src/js/services/logout.js
+++ frontend-margarita/src/js/services/logout.js
@@ -1,10 +1,9 @@
 const logout = async (e) => {
     try {
-        const peticionLogout = fetch('http://localhost:3000/logoutUsers', {
+        const peticionLogout = await fetch('http://localhost:3000/logoutUsers', {
             method: 'POST',
             credentials: 'include'
         })
-        (response => {
             if (peticionLogout.ok) {
                 // Redirige al usuario a la página de inicio de sesión o a la página principal
                 window.location.href = '/login';
@@ -12,7 +11,6 @@ const logout = async (e) => {
                 // Manejar errores
                 console.error('Error al cerrar sesión');
             }
-        })
     } catch (error) {
         console.error('Error en la solicitud de logout', error);
     }
