deb2378a4d3d55cae32c2ff4e69ea7aacf182788
Nahuel <nahuelcarlos150@gmail.com>
2024-09-24 12:19:25 -0300

    se comenzo a hacer la nosessionpage

--- frontend-margarita-react/src/App.jsx
+++ frontend-margarita-react/src/App.jsx
@@ -6,27 +6,29 @@ import { RegisterPage } from './javascript/pages/RegisterPage';
 import { RecoverPage } from './javascript/pages/RecoverPage';
 import { ContactPage } from './javascript/pages/ContactPage';
 import { AboutUsPage } from './javascript/pages/AboutUsPage';
+import {NoSessionPage} from "./javascript/pages/NoSessionPage.jsx"
 function App() {
 
   //Desestructura user y loading del resultado de la función
-  const {user, loading} = authUser()
+  const { user, loading } = authUser()
 
   //Mientras espera la función asincrona, renderiza la pantalla de carga
   if (loading) {
     return (
       <main className="bg-slate-400">
-            <div className="flex justify-center items-center animate-spin rounded-full h-screen w-screen">
-          <img src="img/loadingImage.png" alt="" width="400" height="400"/>
+        <div className="flex flex-col justify-center items-center rounded-full h-screen w-screen">
+          <img src="img/loadingImage.png" alt="" width="400" height="400" className="animate-spin"/>
+          <h2 className='text-3xl text-white'>Cargando</h2>
         </div>
       </main>
     )
   }
-  
+
   //Sino renderiza el sitio
   return (
     <Router>
       <Routes>
-        
+
         <Route path="/login"
           element={user ? <Navigate to={"/home"} /> : <LoginPage />}
         />
@@ -39,11 +41,14 @@ function App() {
         <Route path="/home"
           element={user ? <LandingPage /> : <Navigate to={"/login"} />}
         />
-          <Route path="/contact"
+        <Route path="/contact"
           element={user ? <ContactPage /> : <Navigate to={"/login"} />}
         />
-          <Route path="/aboutus"
+        <Route path="/aboutus"
           element={user ? <AboutUsPage /> : <Navigate to={"/login"} />}
+        />
+        <Route path="/"
+          element = {<NoSessionPage/>}
         />
       </Routes>
     </Router>
---
+++ frontend-margarita-react/src/javascript/pages/NoSessionPage.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Header } from '../components/Header'
+
+export function NoSessionPage() {
+    return (
+        <div>
+            <Header />
+            <div className='flex justify-center items-center h-full'>
+                <div className="relative w-full h-screen flex justify-center">
+                    <div className="absolute top-1/3 transform -translate-y-1/3 w-full h-1/2 bg-yellow-400 z-0 text-white p-6 text-xl">
+                        <div className="mt-10">
+                            Somos una plataforma digital que conecta personas, empresas y <br />
+                            organizaciones que desean hacer un cambio positivo en el mundo <br />
+                            con causas y proyectos que necesitan apoyo. Creemos en el poder <br />
+                            de la comunidad para generar impacto y transformar vidas.<br />
+                            A través de nuestra plataforma, facilitamos la interacción entre <br />
+                            donantes y organizaciones sin fines de lucro, asegurando que cada donación <br />
+                            llegue de manera transparente y efectiva a quienes más lo <br />
+                            necesitan.
+                        </div>
+                    </div>
+
+                    <div className="relative z-10 flex justify-end w-full hidden sm:flex">
+                        <img src="img/FlorNoSession.svg" alt="Not found" className="w-full sm:w-7/12 md:w-10/11 lg:w-6/6" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
