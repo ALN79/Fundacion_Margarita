ee6574844b8c242437b552186a2cb32f3f52b626
Lucas <lucasesteche13@gmail.com>
2024-10-16 16:18:19 -0300

    Se rediseño el aboutus y la pagina de contactos

--- frontend-margarita-react/src/javascript/components/footer.jsx
+++ frontend-margarita-react/src/javascript/components/footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Footer as FlowbiteFooter } from "flowbite-react"; 
 import { BsFacebook, BsInstagram, BsTwitter } from "react-icons/bs"; 
+import { Link } from "react-router-dom";
 
 export function Footer() {
   return (
@@ -18,8 +19,9 @@ export function Footer() {
             <div>
               <FlowbiteFooter.Title title="Nosotros" />
               <FlowbiteFooter.LinkGroup col>
-                <FlowbiteFooter.Link href="#">Nuestra Misión</FlowbiteFooter.Link>
-                <FlowbiteFooter.Link href="#">Nuestro equipo</FlowbiteFooter.Link>
+                {/* Redirige ambos enlaces a la misma página "Acerca de Nosotros" */}
+                <FlowbiteFooter.Link as={Link} to="/aboutus">Nuestra Misión</FlowbiteFooter.Link>
+                <FlowbiteFooter.Link as={Link} to="/aboutus">Nuestro equipo</FlowbiteFooter.Link>
               </FlowbiteFooter.LinkGroup>
             </div>
             <div>
@@ -42,8 +44,8 @@ export function Footer() {
         <FlowbiteFooter.Divider className="border-gray-400 mt-12 mb-6" />
         <div className="flex space-x-6 mt-0 justify-center">
           <FlowbiteFooter.Icon href="#" className="hover:text-blue-600" icon={BsFacebook} />
-          <FlowbiteFooter.Icon href="#"className="hover:text-violet-600"  icon={BsInstagram} />
-          <FlowbiteFooter.Icon href="#"className="hover:text-blue-400" icon={BsTwitter} />
+          <FlowbiteFooter.Icon href="#" className="hover:text-violet-600" icon={BsInstagram} />
+          <FlowbiteFooter.Icon href="#" className="hover:text-blue-400" icon={BsTwitter} />
         </div>
 
         <div className="w-full sm:flex sm:items-center sm:justify-center mt-4">
